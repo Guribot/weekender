8a488c8ba1c897c52a6e9ba0048a59435cb34017
Kate Evans-Spitzer <kateevans1014@gmail.com>
2018-01-08 11:04:16 -0800

    Note Form logic

--- app/src/main/java/com/katespitzer/android/weekender/NoteFormActivity.java
+++ app/src/main/java/com/katespitzer/android/weekender/NoteFormActivity.java
@@ -1,41 +1,112 @@
 package com.katespitzer.android.weekender;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.UUID;
 
 public class NoteFormActivity extends AppCompatActivity {
 
     private Note mNote;
+    private Trip mTrip;
+    private Place mPlace;
+    private NoteList mNoteList;
 
-    private static final String EXTRA_PLACE_DBID = "com.katespitzer.android.weekender.place_db_id";
-    private static final String EXTRA_TRIP_DBID = "com.katespitzer.android.weekender.trip_db_id";
+    private EditText mTitleEditText;
+    private EditText mContentEditText;
+    private Button mSubmitButton;
+
+    private static final String EXTRA_PLACE_ID = "com.katespitzer.android.weekender.place_db_id";
+    private static final String EXTRA_TRIP_ID = "com.katespitzer.android.weekender.trip_db_id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_form);
 
-        int placeId = getIntent().getIntExtra(EXTRA_PLACE_DBID, 0);
-        int tripId = getIntent().getIntExtra(EXTRA_TRIP_DBID, 0);
+        UUID placeId = (UUID) getIntent().getSerializableExtra(EXTRA_PLACE_ID);
+        UUID tripId = (UUID) getIntent().getSerializableExtra(EXTRA_TRIP_ID);
 
-        mNote.setPlaceId(placeId);
-        mNote.setTripId(tripId);
+        if (placeId != null) {
+            mPlace = PlaceList.get(this).getPlace(placeId);
+        }
+
+        mTrip = TripList.get(this).getTrip(tripId);
+
+        mNoteList = NoteList.get(this);
+
+        mTitleEditText = findViewById(R.id.note_title_input);
+        mTitleEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mNote.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mContentEditText = findViewById(R.id.note_content_input);
+        mContentEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mNote.setContent(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mSubmitButton = findViewById(R.id.note_submit_button);
+        mSubmitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (mPlace != null) {
+                    mNoteList.addNoteToPlace(mNote, mPlace);
+                } else {
+                    mNoteList.addNoteToTrip(mNote, mTrip);
+                }
+                finish();
+            }
+        });
+
     }
 
     public Intent newIntent(Place place) {
         Intent intent = new Intent();
-        intent.putExtra(EXTRA_PLACE_DBID, place.getDbId());
-        intent.putExtra(EXTRA_TRIP_DBID, place.getTripId());
+
+        Trip trip = PlaceList.get(this).getTripFor(place);
+
+        intent.putExtra(EXTRA_PLACE_ID, place.getId());
+        intent.putExtra(EXTRA_TRIP_ID, trip.getId());
 
         return intent;
     }
 
     public Intent newIntent(Trip trip) {
         Intent intent = new Intent();
-        intent.putExtra(EXTRA_TRIP_DBID, trip.getDbId());
+        intent.putExtra(EXTRA_TRIP_ID, trip.getId());
 
         return intent;
     }
