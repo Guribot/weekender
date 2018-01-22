9165deb3fb7843272f0e44c6c0decf0cd3585285
Kate Evans-Spitzer <kateevans1014@gmail.com>
2018-01-22 10:46:54 -0800

    add trip, newInstance(), places to map fragmnet

--- app/src/main/java/com/katespitzer/android/weekender/TripMapFragment.java
+++ app/src/main/java/com/katespitzer/android/weekender/TripMapFragment.java
@@ -2,6 +2,7 @@ package com.katespitzer.android.weekender;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,11 +17,40 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.katespitzer.android.weekender.managers.PlaceManager;
+import com.katespitzer.android.weekender.managers.TripManager;
+import com.katespitzer.android.weekender.models.Place;
+import com.katespitzer.android.weekender.models.Trip;
+
+import java.util.List;
+import java.util.UUID;
 
 public class TripMapFragment extends Fragment {
 
     MapView mMapView;
+    private Trip mTrip;
+    private List<Place> mPlaces;
     private GoogleMap googleMap;
+
+    private static final String TAG = "TripMapFragment";
+    private static final String TRIP_ID = "trip_id";
+
+    public Fragment newInstance(UUID tripId) {
+        Fragment fragment = new TripMapFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(TRIP_ID, tripId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        UUID tripId = (UUID) getArguments().getSerializable(TRIP_ID);
+        mTrip = TripManager.get(getActivity()).getTrip(tripId);
+        mPlaces = PlaceManager.get(getActivity()).getPlacesForTrip(mTrip);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,7 +73,10 @@ public class TripMapFragment extends Fragment {
                 googleMap = mMap;
 
                 // For showing a move to my location button
-                if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(getActivity(),
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(getActivity(),
+                                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
                     //    ActivityCompat#requestPermissions
                     // here to request the missing permissions, and then overriding
@@ -58,6 +91,10 @@ public class TripMapFragment extends Fragment {
                 // For dropping a marker at a point on the Map
                 LatLng sydney = new LatLng(-34, 151);
                 googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+//                for (int i = 0; i < mPlaces.size(); i++) {
+//
+//                }
 
                 // For zooming automatically to the location of the marker
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
